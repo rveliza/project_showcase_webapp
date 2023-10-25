619cb460e3b40326d91768fc6cd9692a705a3959
Reyner Veliz <reyner@reynerveliz.com>
2023-10-25 02:13:02 -0600

    Add col1 and col2

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
 
 
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+
+    content = """
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, ad
+"""
+
+    st.info(content)
