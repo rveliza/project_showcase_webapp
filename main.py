7b6ec7d28258982da2daaea07699da7a63f17853
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 14:17:26 -0600

    Render app titles in two columns

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-
 
 with col1:
     st.image("images/photo.png")
@@ -16,3 +16,21 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
 """
 
     st.info(content)
+
+
+contet2 = "iatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt"
+
+st.write(contet2)
+
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
