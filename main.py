bec6aafc157c6c33d9a7cc089803b078a4ead924
Reyner Veliz <reyner@reynerveliz.com>
2023-10-29 14:32:20 -0600

    Add app description, images and links

--- main.py
+++ main.py
@@ -23,14 +23,20 @@ contet2 = "iatis unde omnis iste natus error sit voluptatem accusantium doloremq
 st.write(contet2)
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
